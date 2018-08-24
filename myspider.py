02a107b5924109d028a649f05fbe636fbda28a76
abhinavGirish <abhinav.girish97@gmail.com>
2018-08-24 15:46:46 -0400

    re-doing commit - basic scraper for 538

--- myspider.py
+++ myspider.py
@@ -2,11 +2,15 @@ import scrapy
 
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
-    start_urls = ['https://blog.scrapinghub.com']
+    start_urls = ['https://blog.scrapinghub.com','https://www.vox.com/','https://fivethirtyeight.com/']
 
     def parse(self, response):
-        for title in response.css('.post-header>h2'):
+        """for title in response.css('.post-header>h2'):
             yield {'title': title.css('a ::text').extract_first()}
 
         for next_page in response.css('div.prev-post > a'):
-            yield response.follow(next_page, self.parse)
+            yield response.follow(next_page, self.parse)"""
+        for title in response.css("div.hentry"):
+            yield {'title': title.css('a ::text').extract_first()}
+
+            
