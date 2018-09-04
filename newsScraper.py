927db93717b310ad9f303af6a187975674f1e06b
abhinavGirish <abhinav.girish97@gmail.com>
2018-09-04 14:16:15 -0400

    adding nytimes to list of sites

--- newsScraper.py
+++ newsScraper.py
@@ -4,7 +4,7 @@ class BlogSpider(scrapy.Spider):
     name = 'blogspider'
     start_urls = ['https://blog.scrapinghub.com','https://www.vox.com/',
     'https://fivethirtyeight.com/','https://slate.com/','https://www1.nyc.gov/',
-    'https://www.post-gazette.com/']
+    'https://www.post-gazette.com/','https://www.nytimes.com/']
 
     def parse(self, response):
         #headlines on fivethirtyeight
@@ -22,3 +22,7 @@ class BlogSpider(scrapy.Spider):
         #for handling pittsburgh post-gazette headlines
         for title in response.css("div.pgevoke-textpack-item-text"):
             yield {'title': title.css('span ::text').extract_first()}
+
+        #new york times
+        for title in response.css("div.css-18y7hud.esl82me2"):
+            yield {'title': title.css('h2 ::text').extract_first()}
