26d23827df1c18ef8cd6eb1bb2131d0a6b603eee
abhinavGirish <abhinav.girish97@gmail.com>
2018-10-06 12:43:48 -0400

    adding content scraping for slate

--- democratize_src/democratize_src/spiders/newsScraper.py
+++ democratize_src/democratize_src/spiders/newsScraper.py
@@ -14,27 +14,54 @@ class NewsSpider(scrapy.Spider):
             next_page = title.css('h3 a::attr(href)').extract_first()
             if next_page is not None:
                 next_page = response.urljoin(next_page)
-                yield scrapy.Request(next_page, callback=self.parseArticle)
+                yield scrapy.Request(next_page, callback=self.parseArticleFiveThirtyEight)
 
         #headlines on slate.com
-        for title in response.css("div.story-teaser__teaser"):
+        for title in response.css("section.story-teaser"):
             yield {'title': title.css('h3 ::text').extract_first()}
+            next_page = title.css('section a::attr(href)').extract_first()
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parseArticleSlate)
 
         #for handling nyc govt
         for title in response.css("h2.hero-title"):
             yield {'title': title.css('a ::text').extract_first()}
+            next_page = title.css('h2 a::attr(href)').extract_first()
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parseArticleNYCGov)
 
         #for handling pittsburgh post-gazette headlines
         for title in response.css("div.pgevoke-textpack-item-text"):
             yield {'title': title.css('span ::text').extract_first()}
+            '''next_page = title.css('section a::attr(href)').extract_first()
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parseArticlePPG)'''
 
         #new york times
         for title in response.css("div.css-6p6lnl"):
             yield {'title': title.css('h2 ::text').extract_first()}
 
-    #parsing the text of the article
-    def parseArticle(self, response):
+    #parsing fiveThirtyEight
+    def parseArticleFiveThirtyEight(self, response):
         for section in response.css("div.entry-content.single-post-content"):
             yield {'article_content': section.css('p ::text').extract_first()}
         #yield {'message':"Just entered an article in fivethirtyeight!"}
+
+    def parseArticleSlate(self, response):
+        for section in response.css("div.article__content"):
+            yield {'article_content_slate': section.css('p ::text').extract_first()}
+        #yield {'message':"Just entered an article in SLATE!"}
+
+    def parseArticleNYCGov(self, response):
+        for div in response.css("div.richtext"):
+            yield {'article_content_NYCGov': div.css('p ::text').extract_first()}
+        #yield {'message':"Just entered an article in SLATE!"}
+
+    def parseArticlePPG(self,response):
+        yield {'message':"Just entered an article in PPG"}
+
+
 
